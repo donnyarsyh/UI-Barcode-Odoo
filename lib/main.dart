2e282423a9de003d068ba117aae50e15c6002349
Donny Ardiansyah <donny-ardiansyah@dny.ar>
2025-09-22 16:51:24 +0700

    splash screen & icon

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.white,
         useMaterial3: true,
       ),
-      home: const ListProdukPage(),
+      home: const HomePage(),
     );
   }
 }
